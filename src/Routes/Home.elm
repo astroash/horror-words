3123a9490b74623105ee8b253358c2cf2e01b2ca
astroash <astroaisha@gmail.com>
2017-12-02 22:16:00 +0000

    Dripping navbar with media queries

--- src/Routes/Home.elm
+++ src/Routes/Home.elm
@@ -9,10 +9,10 @@ import Types exposing (..)
 home : Model -> Html Msg
 home model =
     div [ class "w-60-ns center" ]
-        [ h1 [ class "tc f1" ] [ text "hello" ]
-        , p [ class "f3 w60 mh1 tc" ] [ text "Share your horror suggestions" ]
+        [ h1 [ class "tc f1 ma0" ] [ text "⚠️" ]
+        , p [ class "f3 w60 tc ma0" ] [ text "Share your fears" ]
         , Html.form [ class "tc", onSubmit SubmitSuggestion ]
-            [ input [ class "f3 w30 pa1 center tc", onInput Change, value model.userInput, placeholder "the scariest of scares" ] []
-            , button [ class "f3 w30 pa1 center tc" ] [ text "add" ]
+            [ input [ class "f4 w30 ma1 pa1 center tc", onInput Change, value model.userInput, placeholder "the scariest of scares" ] []
+            , button [ class "f4 w30 pa1 center tc" ] [ text "add" ]
             ]
         ]
