0b5a92cb13bf67f7d038ae1bbe33e3c69058593d
astroash <astroaisha@gmail.com>
2017-12-22 18:34:13 -0500

    Decode 3 api calls to movieDb grab first 3 results for movie search partially apply FilmDetails for second api calls fold list to return only director in 3rd api call

--- src/elm/State.elm
+++ src/elm/State.elm
@@ -4,6 +4,9 @@ import Types exposing (..)
 import Ports exposing (..)
 import Json.Decode as Decode
 import Json.Decode.Pipeline as Pipeline
+import Http exposing (..)
+import Task
+import Config exposing (..)
 
 
 -- Model
@@ -12,7 +15,10 @@ import Json.Decode.Pipeline as Pipeline
 model : Model
 model =
     { route = HomeRoute
+    , homePageView = FirstView
     , userInput = ""
+    , filmSearchOptions = []
+    , filmSearchDetail = Nothing
     , suggestions = Nothing
     }
 
@@ -37,6 +43,10 @@ getPage hash =
             HomeRoute
 
 
+
+-- fb decoding
+
+
 decodeSuggestions : Decode.Decoder (List Suggestion)
 decodeSuggestions =
     Decode.list suggestionsDecoder
@@ -50,6 +60,116 @@ suggestionsDecoder =
         |> Pipeline.required "url" Decode.string
 
 
+
+-- movie options decoder
+
+
+filmOptionsDecoder : Decode.Decoder FilmOption
+filmOptionsDecoder =
+    Pipeline.decode FilmOption
+        |> Pipeline.required "title" Decode.string
+        |> Pipeline.required "id" Decode.int
+        |> Pipeline.required "poster_path" Decode.string
+
+
+filmOptionsListDecoder : Decode.Decoder (List FilmOption)
+filmOptionsListDecoder =
+    Pipeline.decode (\a b c -> [ a, b, c ])
+        |> Pipeline.custom (Decode.index 0 filmOptionsDecoder)
+        |> Pipeline.custom (Decode.index 1 filmOptionsDecoder)
+        |> Pipeline.custom (Decode.index 2 filmOptionsDecoder)
+
+
+filmOptionsResultsDecoder : Decode.Decoder (List FilmOption)
+filmOptionsResultsDecoder =
+    Decode.field "results" (filmOptionsListDecoder)
+
+
+
+-- movie options api call
+
+
+filmOptionsApiCall : String -> Cmd Msg
+filmOptionsApiCall film =
+    let
+        url =
+            "https://api.themoviedb.org/3/search/movie?api_key=" ++ apiKey ++ "&query=" ++ film ++ "&page=1&include_adult=false"
+
+        request =
+            Http.get url filmOptionsResultsDecoder
+    in
+        Http.send ReceiveFilmOptions request
+
+
+
+-- movie details decoder
+
+
+filmDetailsDecoder : Decode.Decoder (String -> FilmDetail)
+filmDetailsDecoder =
+    Pipeline.decode FilmDetail
+        |> Pipeline.required "title" Decode.string
+        |> Pipeline.required "release_date" Decode.string
+        |> Pipeline.required "runtime" Decode.int
+        |> Pipeline.required "overview" Decode.string
+        |> Pipeline.required "poster_path" Decode.string
+
+
+itemDecoder : Decode.Decoder ApiCrewMember
+itemDecoder =
+    Pipeline.decode ApiCrewMember
+        |> Pipeline.required "job" Decode.string
+        |> Pipeline.required "name" Decode.string
+
+
+filmDirectorFoldDecoder : Decode.Decoder String
+filmDirectorFoldDecoder =
+    Decode.list itemDecoder
+        |> Decode.andThen
+            (List.foldl
+                (\item acc ->
+                    if item.job == "Director" then
+                        Just item.name
+                    else
+                        acc
+                )
+                Nothing
+                >> Maybe.map Decode.succeed
+                >> Maybe.withDefault (Decode.fail "Item not found!")
+            )
+
+
+filmDirectorDecoder : Decode.Decoder String
+filmDirectorDecoder =
+    Decode.field "crew" (filmDirectorFoldDecoder)
+
+
+filmSelectedApiCall : String -> Cmd Msg
+filmSelectedApiCall filmId =
+    let
+        urlDeets =
+            "https://api.themoviedb.org/3/movie/" ++ filmId ++ "?api_key=" ++ apiKey
+
+        urlDirector =
+            "https://api.themoviedb.org/3/movie/" ++ filmId ++ "/credits?api_key=" ++ apiKey
+
+        request1 =
+            Http.get urlDeets filmDetailsDecoder
+
+        request2 =
+            Http.get urlDirector filmDirectorDecoder
+
+        test =
+            "test"
+    in
+        Task.map2 (,) (Http.toTask request1) (Http.toTask request2)
+            |> Task.attempt ReceiveFilmDetails
+
+
+
+--  `(,)` is equivalent to `(\r1 r2 -> (r1, r2))`. It takes two values and puts them in a tuple
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case Debug.log "update" msg of
@@ -59,10 +179,13 @@ update msg model =
         UrlChange location ->
             { model | route = (getPage location.hash) } ! [ Cmd.none ]
 
-        SubmitFilmSearch ->
-            ( model, Cmd.none )
+        SubmitInitialFilmSearch film ->
+            ( model, filmOptionsApiCall film )
 
-        SubmitSuggestion string ->
+        SubmitSelectedFilm filmId ->
+            ( model, filmSelectedApiCall filmId )
+
+        SubmitSuggestionToDb string ->
             ( model, changeSuggestions string )
 
         GotSuggestions json ->
@@ -71,4 +194,46 @@ update msg model =
                     Decode.decodeValue decodeSuggestions (Debug.log "json" json)
                         |> Result.toMaybe
             in
-                ( { model | suggestions = Debug.log "decoder" newSuggestions }, Cmd.none )
+                ( { model | suggestions = newSuggestions }, Cmd.none )
+
+        ReceiveFilmOptions (Ok data) ->
+            ( { model | filmSearchOptions = data, homePageView = FilmOptionsView }, Cmd.none )
+
+        ReceiveFilmOptions (Err err) ->
+            let
+                log =
+                    Debug.log "ERR" err
+            in
+                ( model, Cmd.none )
+
+        ReceiveFilmDetails (Ok ( result, result2 )) ->
+            let
+                mergeDetail =
+                    result result2
+
+                sanitisedYear =
+                    case (sanitiseDate mergeDetail.year) of
+                        Just year ->
+                            year
+
+                        Nothing ->
+                            "Unknown"
+
+                filmDetail =
+                    { mergeDetail | year = sanitisedYear }
+            in
+                ( { model | filmSearchDetail = Just filmDetail, homePageView = FilmDetailsView }, Cmd.none )
+
+        ReceiveFilmDetails (Err err) ->
+            let
+                log =
+                    Debug.log "ERR" err
+            in
+                ( model, Cmd.none )
+
+
+sanitiseDate : String -> Maybe String
+sanitiseDate strDate =
+    strDate
+        |> String.split "-"
+        |> List.head
--- src/elm/Types.elm
+++ src/elm/Types.elm
@@ -2,6 +2,7 @@ module Types exposing (..)
 
 import Navigation
 import Json.Encode as Json
+import Http
 
 
 -- Model
@@ -15,9 +16,19 @@ type Route
 
 type alias Model =
     { route : Route
+    , homePageView : HomePageView
     , userInput : String
+    , filmSearchOptions : List FilmOption
+    , filmSearchDetail : Maybe FilmDetail
     , suggestions : Maybe (List Suggestion)
     }
+
+
+type HomePageView
+    = FirstView
+    | FilmOptionsView
+    | FilmDetailsView
+    | FilmSuggestionsView
 
 
 type alias Suggestion =
@@ -29,21 +40,39 @@ type alias Suggestion =
     }
 
 
+type alias FilmDetail =
+    { title : String
+    , year : String
+    , runtime : Int
+    , plot : String
+    , poster : String
+    , director : String
+    }
 
--- type alias FilmDetails =
---     { title : String
---     , year : String
---     , runtime : String
---     , director : String
---     , plot : String
---     , poster : String
---     }
+
+type alias FilmOption =
+    { title : String
+    , filmId : Int
+    , posterPath : String
+    }
+
+
+type alias ApiCrewMember =
+    { job : String
+    , name : String
+    }
+
+
+
 -- Update
 
 
 type Msg
     = Change String
     | UrlChange Navigation.Location
-    | SubmitFilmSearch
-    | SubmitSuggestion String
+    | SubmitInitialFilmSearch String
+    | SubmitSelectedFilm String
+    | SubmitSuggestionToDb String
     | GotSuggestions Json.Value
+    | ReceiveFilmOptions (Result Http.Error (List FilmOption))
+    | ReceiveFilmDetails (Result Http.Error ( String -> FilmDetail, String ))
