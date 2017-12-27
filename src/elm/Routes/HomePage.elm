9438766149fcdd88bb80e6a52d943882fff1a88a
astroash <astroaisha@gmail.com>
2017-12-27 10:53:19 -0500

    :sparkles: Add search on input relates #9 #19

--- src/elm/Routes/HomePage.elm
+++ src/elm/Routes/HomePage.elm
@@ -48,9 +48,8 @@ homePage model =
         div [ class "w-80-ns w-60-m center" ]
             [ h1 [ class "tc f1 ma0" ] [ text "⚠️" ]
             , p [ class "f3 w60 tc ma0" ] [ text "Share your fears" ]
-            , Html.form [ class "tc", onSubmit <| SubmitInitialFilmSearch model.userInput ]
+            , Html.form [ class "tc", onInput SubmitInitialFilmSearch ]
                 [ input [ class "f4 w30 ma1 pa1 center tc", onInput Change, value model.userInput, placeholder "the scariest of scares" ] []
-                , button [ class "f4 w30 pa1 center tc" ] [ text "add" ]
                 ]
             , changeView
             ]
